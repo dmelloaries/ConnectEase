3b8563e2bb348df8e526d84c09826b0a006c2891
Aries Dmello <dmelloaries@gmail.com>
2024-08-01 23:44:59 +0530

    Backend setup done

---
+++ backend/server.js
@@ -0,0 +1,26 @@
+require('dotenv').config(); 
+const express = require('express');
+const mongoose = require('mongoose');
+const cors = require('cors');
+const bodyParser = require('body-parser');
+
+const contactRoutes = require('./routes/contactRoutes');
+const emailRoutes = require('./routes/emailRoutes');
+
+const app = express();
+app.use(cors());
+app.use(bodyParser.json());
+
+
+const mongoUrl = process.env.MONGO_URL;
+
+mongoose.connect(mongoUrl, {
+    useNewUrlParser: true,
+    useUnifiedTopology: true
+});
+
+app.use('/contacts', contactRoutes);
+app.use('/send', emailRoutes);
+
+const PORT = process.env.PORT || 5000;
+app.listen(PORT, () => console.log(`Server running on port ${PORT}`));
